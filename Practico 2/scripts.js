8c130614d2be6b378a21d6e363bf98e207d80de0
Martin Najle <martin.najle@gmail.com>
2013-04-21 21:02:02 -0300

    Some fixes

--- Practico 2/scripts.js
+++ Practico 2/scripts.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
-    alert("Page has finished loading");
+    alert('Page has finished loading');
 
     $('#page1').keydown(function(e){
         if(e.which == 27){
             $.mobile.loading( 'hide' );
-            console.log("stop spinner event invoked with ESC");
+            console.log('stop spinner event invoked with ESC');
         }
     });
      loadTweets();
@@ -17,7 +17,7 @@ $(document).ready(function() {
     function loadTweets(){
 
       $.ajax({
-          url: "http://search.twitter.com/search.json?q=html5&rpp=5&include_entities=true&result_type=mixed",
+          url: 'http://search.twitter.com/search.json?q=html5&rpp=5&include_entities=true&result_type=mixed',
           type: 'GET',
           dataType: 'jsonp',
           context: document.body,
@@ -25,27 +25,27 @@ $(document).ready(function() {
             $('.background').show();
             results = data.results;
             var $list = $('#tweetsList');
-            $list.on("click", "li",function(e){
-               var currentItem= ($(this).attr('id')).split("_");
-               $('#item').html('<p>'+results[currentItem[1]].text+'</p>');
+            $list.on('click', 'li',function(e){
+               var currentItem= ($(this).attr('id')).split('_');
+               $('#item').html('<img  width="200" height="150" src="' + results[currentItem[1]].profile_image_url +'"></img><p>'+results[currentItem[1]].text+'</p>');
                  
              });
-            var tweet_data =""; //"<h1>Tweets list</h1>";
+            var tweet_data= ''; //"<h1>Tweets list</h1>";
 
                 for (var i = 0; i < results.length; i++) {
-                    tweet_data += "<li id='item_"+i+"'><a href='#dialogTweet' data-rel='dialog'>Tweet " + i + ": <br>";
-                    tweet_data += "From_user: " + results[i].from_user + "<br>";
-                    tweet_data += "Text: " + results[i].text + "<br>";
-                    tweet_data += "Created at: " + results[i].created_at + "<br>";
-                    tweet_data += "<img src='" + results[i].profile_image_url + "'title= 'Profile_image_url: "+ results[i].profile_image_url+ "'></img></a></li>";
+                    tweet_data += '<li id="item_'+i+'"><a href="#dialogTweet" data-rel="dialog">Tweet (' + i + '): <br>';
+                    tweet_data += 'From_user: ' + results[i].from_user + '<br>';
+                    tweet_data += 'Text: ' + results[i].text + '<br>';
+                    tweet_data += 'Created at: ' + results[i].created_at + '<br>';
+                    tweet_data += '<img  src="' + results[i].profile_image_url +'"></img></a></li>';
                 }
                 $list.append(tweet_data);
-          $("#tweetsList").listview("refresh"); // This line now updates the listview
+          $('#tweetsList').listview('refresh'); // This line now updates the listview
 
 
           },
           error: function() {
-            console.log("An error occurred in the service call");
+            console.log('An error occurred in the service call');
           }
         })
     }
--- Practico 3/movie.js
+++ Practico 3/movie.js
@@ -3,9 +3,12 @@ function Movie() {
    this.play = function(){
      var movieName= attributes['name'] || 'Movie undefined';
      console.log(movieName+' Now is playing...');
+     return movieName +' Now is playing...';
    };
    this.stop = function(){
+    var movieName= attributes['name'] || 'Movie undefined';
    	console.log("Stopping the movie...");
+    return movieName + " stopped, the movie...";
    };
    this.get = function(key){
      return attributes[key];
