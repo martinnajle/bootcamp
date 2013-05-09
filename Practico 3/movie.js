110c0106153f763953b4d26609ba9d327727ded9
Martin Najle <martin.najle@globant.com>
2013-05-09 15:51:47 -0300

    movies

--- Practico 3/movie.js
+++ Practico 3/movie.js
@@ -1,19 +1,19 @@
 function Movie() {
    var attributes={};
-   this.play = function(){
+   this.play = function() {
      var movieName= attributes['name'] || 'Movie undefined';
      console.log(movieName+' Now is playing...');
      return movieName +' Now is playing...';
    };
-   this.stop = function(){
+   this.stop = function() {
     var movieName= attributes['name'] || 'Movie undefined';
    	console.log("Stopping the movie...");
     return movieName + " stopped, the movie...";
    };
-   this.get = function(key){
+   this.get = function(key) {
      return attributes[key];
    };
-   this.set = function(key, value){
+   this.set = function(key, value) {
    	 attributes[key]=value;
    };
 }
--- Practico 3/scripts.js
+++ Practico 3/scripts.js
@@ -1,11 +1,13 @@
-$(document).ready(function(){
+$(document).ready(function() {
   var peli= new Movie();
   peli.set('name','Hansel and Gretel');
-  $('#play').on('click', function(){
+
+  $('#play').on('click', function() {
   	 $('#message').html(peli.play())
   	 $('#stop').removeAttr("disabled");
   });
-  $('#stop').on('click', function(){
+
+  $('#stop').on('click', function() {
     $('#message').html(peli.stop())
     $('#stop').attr("disabled","disabled");
   });
